8c066d109b1b6d0bbc120febb8f48bd2de8dbf4b
lion1230 <skgmldnjs8080@gmail.com>
2026-08-18 16:29:04 +0900

    2-10 상세 페이지 만들기

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,8 +2,10 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.mysite.sbb.DataNotFoundException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +14,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
