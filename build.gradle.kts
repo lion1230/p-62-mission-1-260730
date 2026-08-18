78f5b50b659aefca2e11ff134bcbe65735787167
lion1230 <skgmldnjs8080@gmail.com>
2026-08-18 21:42:13 +0900

    2-16 질문 등록 기능 추가하기

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-h2console")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 tasks.withType<Test> {
